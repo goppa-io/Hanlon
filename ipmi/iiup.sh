6994254482328972fba9f01aad39265542d7301c
DLX <denver@ii.org.nz>
2016-11-26 11:13:27 +1300

    Add ii_up Function for IPMI

---
+++ ipmi/iiup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+function ii_up {
+    for name in ${IPMI_HOSTS}; do
+        eval local command="\"$1\""
+        eval local node_args=\"\$IPMI_HOST_${name}\"
+        eval local user_args=\"\$IPMI_USERS_${name}\"
+        eval local pass_args=\"\$IPMI_PASS_${name}\"
+        ipmitool -I lanplus -U "${user_args}" -P "${pass_args}" -H "${node_args}" ${command}
+        done
+}
