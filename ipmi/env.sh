60a7698ff87231e097768db84e357b4ef1f628c2
DLX <denver@ii.org.nz>
2016-11-26 11:22:45 +1300

    Add Example ENV

---
+++ ipmi/env.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export IPMI_HOSTS="infra0 infra1 infra2"
+
+export IPMI_HOST_infra0=192.168.1.200
+export IPMI_HOST_infra1=192.168.1.201
+export IPMI_HOST_infra2=192.168.1.202
+
+export IPMI_USER_infra0=admin
+export IPMI_USER_infra1=admin
+export IPMI_USER_infra1=admin
+
+export IPMI_PASS_infra0=P@ssw0rd
+export IPMI_PASS_infra1=P@ssw0rd
+export IPMI_PASS_infra1=P@ssw0rd
+
+
--- ipmi/iiup.sh
+++ ipmi/iiup.sh
@@ -4,7 +4,7 @@ function ii_up {
     for name in ${IPMI_HOSTS}; do
         eval local command="\"$1\""
         eval local node_args=\"\$IPMI_HOST_${name}\"
-        eval local user_args=\"\$IPMI_USERS_${name}\"
+        eval local user_args=\"\$IPMI_USER_${name}\"
         eval local pass_args=\"\$IPMI_PASS_${name}\"
         ipmitool -I lanplus -U "${user_args}" -P "${pass_args}" -H "${node_args}" ${command}
         done
