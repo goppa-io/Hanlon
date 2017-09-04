402ec9a738eef600a43bd694d83316b0209dbf8c
dlx <denver@ii.coop>
2017-09-04 14:15:04 +1200

    Debian Preseed should use hostname for url

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@
 
 MODEL_UUID=$(./cli/hanlon model add -t debian_wheezy -l debian-jessie -i TikgPUUjKJ4sG4Xcxt0le -o debian.yml | awk 'FNR == 6 {print $3}')
 
-./cli/hanlon policy add -p linux_deploy -l Core -m 2CHD2ud1tc7LWTUi2WqB6a -b none -t MXQ42100L6 -e true
+./cli/hanlon policy add -p linux_deploy -l Core -m $MODEL_UUID -b none -t MXQ42100L6 -e true
