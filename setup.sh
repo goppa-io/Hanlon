73e0ba23991aba0eb0db0a3cf1397ac4b165d559
dlx <denver@ii.coop>
2017-09-04 13:29:17 +1200

    Setup.sh should be dynamic

--- setup.sh
+++ setup.sh
@@ -2,8 +2,6 @@
 
 ./cli/hanlon tag add -n 'serialnumber' -f serialnumber
 
-./cli/hanlon model add -t debian_wheezy -l debian-jessie -i TikgPUUjKJ4sG4Xcxt0le
-
-./cli/hanlon model add -t debian_wheezy -l debian-jessie -i TikgPUUjKJ4sG4Xcxt0le -o debian.yml
+MODEL_UUID=$(./cli/hanlon model add -t debian_wheezy -l debian-jessie -i TikgPUUjKJ4sG4Xcxt0le -o debian.yml | awk 'FNR == 6 {print $3}')
 
 ./cli/hanlon policy add -p linux_deploy -l Core -m 2CHD2ud1tc7LWTUi2WqB6a -b none -t MXQ42100L6 -e true
